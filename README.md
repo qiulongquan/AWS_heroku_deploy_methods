065d2aa7555366c93baf5654de1db6c9e6dc0a7d
qiulongquan <2215804749@qq.com>
2020-09-26 19:44:09 +0900

    手势识别project

---
+++ README.md
@@ -0,0 +1,18 @@
+```
+AWS,heroku平台上部署程序及部署文件写法
+
+hand_aws
+这个部署在aws上面的手势识别的简化测试版本
+
+hand_heroku
+这个是部署在heroku上面的手势识别的简化测试版本
+
+heroku
+--mysite  这个里面是部署在heroku上面的手势识别的完整版（已经不需要了，使用AWS的EC2来部署更好）
+--python-getting-started  这个是heroku的demo程序，主要是查看配置文件的写法
+--test_pic_files  这个是测试用的图片文件夹
+
+Books_Manage_django_file_upload_show_prediction
+--mysite_aws  这个是部署在aws上面的手势识别的完整版，已经在AWS的EC2上面部署成功，今后使用Docker的容器来部署各种APP
+
+```
---
+++ hand_heroku/mysite/wsgi.py
@@ -0,0 +1,23 @@
+"""
+WSGI config for mysite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+application = get_wsgi_application()
+
+#
+# def application(environ, start_response):
+#     if environ['mod_wsgi.process_group'] != '':
+#         import signal
+#         os.kill(os.getpid(), signal.SIGINT)
+#     return ["killed"]
